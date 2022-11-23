46b5c0b00e5379ee8f2992b023d247db09f40a36
Stephane Leandre Paul <108344476+stephanethps@users.noreply.github.com>
2022-11-23 09:41:37 -0500

    chore: database_cleaner setup

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# DatabaseCleaner setup
+RSpec.configure do |config|
+  config.before(:suite) do 
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :trsansaction
+  end
+
+  config.after do
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
