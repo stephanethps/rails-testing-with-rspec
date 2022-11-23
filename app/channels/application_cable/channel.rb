e29fd0555edff59849e2a25b908ef92dda4a3c06
Stephane Leandre Paul <108344476+stephanethps@users.noreply.github.com>
2022-11-23 09:28:13 -0500

    chore: setup and rubocop

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Application cable
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
   end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Application cable
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
   end
